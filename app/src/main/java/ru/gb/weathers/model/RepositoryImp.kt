fd33c04d40239f06f1cd32fb614b084e005395ef
neqelovek <neqelovek@gmail.com>
2022-02-27 14:32:29 +0300

    Lesson4 start

--- app/src/main/java/ru/gb/weathers/model/RepositoryImp.kt
+++ app/src/main/java/ru/gb/weathers/model/RepositoryImp.kt
@@ -1,19 +1,8 @@
 package ru.gb.weathers.model
 
 class RepositoryImp : Repository {
-    override fun getWeatherFromServer(): Weather {
-        return Weather()
-    }
-
-    override fun getHomeWeather(): Weather {
-        return Weather()
-    }
-
-    override fun getWeatherFromLocalStorageRus(): List<Weather> {
-        return getRussianCities()
-    }
-
-    override fun getWeatherFromLocalStorageWorld(): List<Weather> {
-        return getWorldCities()
-    }
+    override fun getWeatherFromServer() = Weather()
+    override fun getHomeWeather() = Weather()
+    override fun getWeatherFromLocalStorageRus() = getRussianCities()
+    override fun getWeatherFromLocalStorageWorld() = getWorldCities()
 }
